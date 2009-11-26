89cfc8ef1002985ce0c90d4398ae39b2c3b1589e
myshen <myshen@synmantx.(none)>
2009-11-26 02:02:15 -0800

    Make home page updating map nice with animations

--- app/webroot/js/home.js
+++ app/webroot/js/home.js
@@ -1,77 +1,72 @@
-var PROJC = PROJC ? PROJC : {};
-PROJC.pics = [];
-PROJC.picMarker = function(pic) {
-  var markerOpts = {
-    title: pic.User.name+': '+pic.caption,
-    position: new google.maps.LatLng(pic.lat, pic.lng),
-    icon: new google.maps.MarkerImage('pictures/'+pic.id/*,
-      new google.maps.Size(pic.width, pic.height)*/
-    )
-  }
-  return new google.maps.Marker(markerOpts);
+var LG = LG ? LG : {};
+LG.HOME = {};
+LG.HOME.TIME_BETWEEN = 5000;
+LG.HOME.MAX_PICS_IN_STREAM = 8;
+LG.HOME.map = null;
+/* Overlay displays a caption bubble over the map with the user, caption,
+ * location, and time. Picture is displayed over the center of the map which
+ * centers on the location. Picture is also faded into the right-most empty
+ * spot of the stream. If there's too many, bump out the left-most picture.
+ */
+LG.HOME.QueueOverlay = function() {
+  this.captionPane = $('<div id="map_caption"></div>');
+  this.picturePane = $('<div id="map_picture"></div>');
+  this.stream = $('#updating_map_stream');
+  this.picturePane.children('img').css({'width': '50px', 'height': '50px'});
 };
-PROJC.mapAddPic = function(map, pic) {
-  var marker = PROJC.picMarker(pic);
-  marker.setMap(map);
-  PROJC.pics.push(marker);
-};
-
-PROJC.HOME = {};
-PROJC.HOME.map = null;
-var overlay = PROJC.HOME.overlay = function() {
-  this.captionPane = $('<div id="map_caption"><img src="/img/mini_pic.jpg" /><a href="#">username</a><p>Just saw a wicked accident on the freeway. Hope everyone is okay.</p><div class="location">San Diego, CA</div><div class="time">6 seconds ago</div></div>');
-};
-overlay.prototype = new google.maps.OverlayView();
-overlay.prototype.onAdd = function() {
+LG.HOME.QueueOverlay.prototype = new google.maps.OverlayView();
+LG.HOME.QueueOverlay.prototype.onAdd = function() {
   $(this.getMap().getDiv()).append(this.captionPane);
-  var up = $('#updating_map_stream');
-  up.append($('<img src="/img/db/8.jpg" />'));
-  up.append($('<img src="/img/db/7.jpg" />'));
-  up.append($('<img src="/img/db/5.jpg" />'));
-  up.append($('<img src="/img/db/5.jpg" />'));
-  up.append($('<img src="/img/db/3.jpg" />'));
-  up.append($('<img src="/img/db/3.jpg" />'));
-  up.append($('<img src="/img/db/2.jpg" />'));
+  $(this.getMap().getDiv()).append(this.picturePane);
 };
-overlay.prototype.draw = function() {
-  this.captionPane;
+LG.HOME.QueueOverlay.prototype.draw = function() {
 };
-overlay.prototype.onRemove = function() {
-  //remove obj from dom
-  $(this.getPanes().floatPane).empty();
+LG.HOME.QueueOverlay.prototype.onRemove = function() {
+  $(this.getMap().getDiv()).empty();
 };
-PROJC.HOME.switchPic = function(pic) {
-  if (PROJC.pics.length > 0) {
-    PROJC.pics.pop().map = null;
+LG.HOME.QueueOverlay.prototype.addPic = function(pic) {
+  var self = this;
+  function appendStream() {
+    self.getMap().panTo(new google.maps.LatLng(pic.lat, pic.lng));
+    var caption = '<img src="/img/mini_pic.jpg" /><a href="#">'+
+      pic.User.name+'</a><p>'+pic.caption+
+      '</p><div class="location">San Diego, CA</div><div class="time">6 seconds ago</div>';
+    self.captionPane.hide().html(caption).fadeIn(600);
+    self.picturePane.hide().html('<img src="/img/db/'+pic.id+'.jpg" />').fadeIn(600);
+    self.stream.children().fadeTo(600, 0.5);
+    self.stream.append($('<img src="/img/db/'+pic.id+'.jpg" />').fadeIn(600));
   }
-  PROJC.mapAddPic(PROJC.HOME.map, pic);
+  if (this.stream.children().length >= LG.HOME.MAX_PICS_IN_STREAM) {
+    $(this.stream.children()[0]).hide(300, function() {
+      $(this).remove();
+      appendStream();
+    });
+  } else {
+    appendStream();
+  }
 };
-function cyclesamplepics() {
-  jQuery.each(PROJC.recentPictures, function() {
-    PROJC.pics.push(PROJC.picMarker(this));
-  });
+function cycleSamplePics() {
   function swappic() {
-    var oldpic = PROJC.pics[0];
-    oldpic.setMap(null);
-    PROJC.pics.push(PROJC.pics.shift());
-    PROJC.pics[0].setMap(PROJC.HOME.map);
-    PROJC.HOME.map.panTo(PROJC.pics[0].getPosition());
-    setTimeout(swappic, 3000);
+    PROJC.recentPictures.push(PROJC.recentPictures.shift());
+    LG.HOME.queue.addPic(PROJC.recentPictures[0]);
+    setTimeout(swappic, LG.HOME.TIME_BETWEEN);
   }
   swappic();
 }
-PROJC.HOME.init = function() {
+LG.HOME.init = function() {
   var mapOpts = {
     zoom: 7,
     center: new google.maps.LatLng(32.77977,-117.137947),
     mapTypeId: google.maps.MapTypeId.TERRAIN,
+    draggable: false,
+    disableDoubleClickZoom: true,
     disableDefaultUI: true,
     mapTypeControl: false
   };
-  PROJC.HOME.map = new google.maps.Map($("#map")[0], mapOpts);
-  cyclesamplepics();
-  var OVERLAY = new PROJC.HOME.overlay();
-  OVERLAY.setMap(PROJC.HOME.map);
-};
+  LG.HOME.map = new google.maps.Map($("#map")[0], mapOpts);
+  LG.HOME.queue = new LG.HOME.QueueOverlay();
+  LG.HOME.queue.setMap(LG.HOME.map);
 
-$(document).ready(function(){PROJC.HOME.init();});
+  cycleSamplePics();
+};
+$(document).ready(function(){LG.HOME.init();});
